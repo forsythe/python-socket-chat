11232f876f38930dd058697a810c80e4dbeef92d
forsythe <fjorsyth@gmail.com>
2018-03-13 20:05:32 +0800

    test

---
+++ client.py
@@ -0,0 +1,39 @@
+from socket import AF_INET, SOCK_STREAM, socket
+from threading import Thread
+
+HOST = input('Enter host: ')
+PORT = input('Enter port: ')
+if not PORT:
+	PORT = 12000
+else:
+	PORT = int(PORT)
+
+if not HOST:
+	HOST = 'localhost'
+
+BUFSIZ = 1024
+SERVER_ADDR = (HOST, PORT)
+
+def receive():
+	while True:
+		try:
+			msg = client_socket.recv(BUFSIZ).decode()
+			print(msg)
+		except OSError:
+			break
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(SERVER_ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+
+while True:
+	msg = input()
+	client_socket.send(msg.encode())
+
+	if msg == ".QUIT":
+		client_socket.close()
+		break
+
+print("See you again soon!")
---
+++ server.py
@@ -0,0 +1,59 @@
+from socket import AF_INET, SOCK_STREAM, socket
+from threading import Thread
+
+clients = {}
+addresses = {}
+
+HOST = ''
+PORT = 12000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+def accept_incoming_connections():
+	# Sets up handling for incoming clients.
+	while True:
+		client, client_address = SERVER.accept()
+		print("{} connecting...".format(client_address))
+		client.send("Enter username: ".encode())
+		addresses[client] = client_address
+		
+		#func in new thread
+		Thread(target=handle_client, args=(client,)).start() 
+
+def handle_client(client):  # Takes client socket as argument.
+	# Handles a single client connection.
+	name = client.recv(BUFSIZ).decode()
+	client.send("Welcome {}! Type .QUIT to quit".format(name).encode())
+	incoming_msg = "{} connected as {}.".format(client.getpeername(),name)
+	print(incoming_msg)
+	broadcast(incoming_msg)
+	clients[client] = name
+	while True:
+		msg = client.recv(BUFSIZ).decode()
+		if msg.strip() != ".QUIT":
+			broadcast(msg, prefix=name+": ")
+		else:
+			#client.send(".QUIT".encode())
+			client.close()
+			del clients[client]
+			dc_msg = "{} disconnected.".format(name)
+			print(dc_msg)
+			broadcast(dc_msg)
+			break
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+	# Broadcasts a message to all the clients.
+	for sock in clients:
+		completeMsg = prefix+msg
+		sock.send(completeMsg.encode())			
+
+if __name__ == "__main__":
+	SERVER.listen(5)  # Listens for 5 connections at max.
+	print("Waiting for connection...")
+	ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+	ACCEPT_THREAD.start()  # Starts the infinite loop.
+	ACCEPT_THREAD.join()
+	SERVER.close()
